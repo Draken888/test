23b41596a9396c93acc2f09b553545d05113affc
Draken <drakenalimov@gmail.com>
2022-09-21 21:06:32 +0300

    leo

---
+++ script.js
@@ -0,0 +1,28 @@
+var name = prompt('Введите своё имя ');
+var age = +prompt('Введите свой возраст ');
+
+console.log(name + age);
+var add = +prompt('9 + 2 = ?')
+
+console.log('ваш ответ ' + add);
+console.log('правильный ответ ' + (9 + 2));
+
+var leo = +prompt('8 - 2 = ?')
+
+console.log('Ваш ответ ' + leo);
+console.log('правильный ответ ' + (8-2));
+
+var str = +prompt('9 * 2 = ?')
+
+console.log('Ваш ответ ' + str);
+console.log('правильный ответ ' + (9 * 2));
+
+var poor = +prompt('8 / 2 = ?')
+
+console.log('Ваш ответ ' + poor);
+console.log('правильный ответ ' + (8 / 2));
+
+var div = +prompt('Остаток от 11 / 2')
+
+console.log('Ваш ответ ' + div);
+console.log('правильный ответ ' + (11 % 2));
